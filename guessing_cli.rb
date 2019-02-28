a05784799370a618eef48cd8863e6d58d073e76b
Lige <lige.williamson@gmail.com>
2019-02-28 01:30:41 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,10 +1,11 @@
 # Code your solution here!
+require 'pry'
 def run_guessing_game
   puts "Guess a number between 1 and 6."
 
   mystery_number = rand(1..6)
   input = gets.chomp
-
+  binding.pry
   if input == 'exit'
     puts 'Goodbye!'
   elsif input.to_i.between?(1, 6)

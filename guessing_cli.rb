8acfb6f05e92c4f13586fd9687cb45ce6298bdc5
Lige <lige.williamson@gmail.com>
2019-02-28 01:18:45 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,4 +1,5 @@
 # Code your solution here!
+require 'pry'
 def run_guessing_game
   puts "Guess a number between 1 and 6."
 
@@ -13,4 +14,6 @@ def run_guessing_game
 
   puts "Goodbye!"
 end
+
+binding.pry
   
